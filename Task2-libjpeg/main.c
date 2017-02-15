4386d7480b27ace38729fcae94f67bb315fbcaf1
David Bourke <bourkeda@lg12l36.scss.tcd.ie>
2017-02-15 10:24:09 +0000

    added test images

--- Task2-libjpeg/main.c
+++ Task2-libjpeg/main.c
@@ -1,8 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>		// stat(...)
-#include <syslog.h>
+#include <unistd.h>
 #include <sys/stat.h>
 
 #include <jpeglib.h>
@@ -19,14 +18,10 @@
 int main (int argc, char *argv[]) {
 	int rc, i, j;
 
-	char *syslog_prefix = (char*) malloc(1024);
-	sprintf(syslog_prefix, "%s", argv[0]);
-	openlog(syslog_prefix, LOG_PERROR | LOG_PID, LOG_USER);
-
 	// Check that user has actually provided a filepath as an argument
 	if (argc != 2) {
 		fprintf(stderr, "USAGE: %s filename.jpg\n", argv[0]);
-		exit(EXIT_FAILURE);
+		return 1;
 	}
 
 	// Setup for decompression starts here
@@ -36,7 +31,7 @@ int main (int argc, char *argv[]) {
 	unsigned long jpg_size;
 	unsigned char *jpg_buffer;
 
-	// Variables for the decompressor itself
+	// decompressor object declarations
 	struct jpeg_decompress_struct cinfo;
 	struct jpeg_error_mgr jerr;
 
@@ -46,15 +41,11 @@ int main (int argc, char *argv[]) {
 	int row_stride, width, height, pixel_size;
 
 
-	// Load the jpeg data from a file into a memory buffer for 
-	// the purpose of this demonstration.
-	// Normally, if it's a file, you'd use jpeg_stdio_src, but just
-	// imagine that this was instead being downloaded from the Internet
-	// or otherwise not coming from disk
+	// Load the jpeg data from a file into a memory buffer
 	rc = stat(argv[1], &file_info);
 	if (rc) {
-		syslog(LOG_ERR, "FAILED to stat source jpg");
-		exit(EXIT_FAILURE);
+		printf("ERROR: File %s, not found or could not be opened\n", argv[1]);
+		return 1;	// return 1 for ERROR
 	}
 	jpg_size = file_info.st_size;
 	jpg_buffer = (unsigned char*) malloc(jpg_size + 100);	// 100 bytes padding to avoid accidental segfault
@@ -63,14 +54,12 @@ int main (int argc, char *argv[]) {
 	i = 0;
 	while (i < jpg_size) {
 		rc = read(fd, jpg_buffer + i, jpg_size - i);
-		syslog(LOG_INFO, "Input: Read %d/%lu bytes", rc, jpg_size-i);
 		i += rc;
 	}
 	close(fd);
 
 	// Decompression starts here
 
-	syslog(LOG_INFO, "Proc: Create Decompress struct");
 	// Allocate a new decompress struct, with the default error handler.
 	// The default error handler will exit() on pretty much any issue,
 	// so it's likely you'll want to replace it or supplement it with
@@ -78,32 +67,20 @@ int main (int argc, char *argv[]) {
 	cinfo.err = jpeg_std_error(&jerr);	
 	jpeg_create_decompress(&cinfo);
 
-
-	syslog(LOG_INFO, "Proc: Set memory buffer as source");
-	// Configure this decompressor to read its data from a memory 
-	// buffer starting at unsigned char *jpg_buffer, which is jpg_size
-	// long, and which must contain a complete jpg already.
-	//
-	// If you need something fancier than this, you must write your 
-	// own data source manager, which shouldn't be too hard if you know
-	// what it is you need it to do. See jpeg-8d/jdatasrc.c for the 
-	// implementation of the standard jpeg_mem_src and jpeg_stdio_src 
-	// managers as examples to work from.
+	// Configure the decompressor object cinfo 
+	// to read its data from jpeg_buffer 
+	// jpg_size is the size of the buffer
 	jpeg_mem_src(&cinfo, jpg_buffer, jpg_size);
 
-
-	syslog(LOG_INFO, "Proc: Read the JPEG header");
 	// Have the decompressor scan the jpeg header. This won't populate
 	// the cinfo struct output fields, but will indicate if the
 	// jpeg is valid.
 	rc = jpeg_read_header(&cinfo, TRUE);
 
 	if (rc != 1) {
-		syslog(LOG_ERR, "File does not seem to be a normal JPEG");
 		exit(EXIT_FAILURE);
 	}
 
-	syslog(LOG_INFO, "Proc: Initiate JPEG decompression");
 	// By calling jpeg_start_decompress, you populate cinfo
 	// and can then allocate your output bitmap buffers for
 	// each scanline.
@@ -113,9 +90,6 @@ int main (int argc, char *argv[]) {
 	height = cinfo.output_height;
 	pixel_size = cinfo.output_components;
 
-	syslog(LOG_INFO, "Proc: Image is %d by %d with %d components", 
-			width, height, pixel_size);
-
 	bmp_size = width * height * pixel_size;
 	bmp_buffer = (unsigned char*) malloc(bmp_size);
 
@@ -123,21 +97,6 @@ int main (int argc, char *argv[]) {
 	// entire scanline (row). 
 	row_stride = width * pixel_size;
 
-
-	syslog(LOG_INFO, "Proc: Start reading scanlines");
-	//
-	// Now that you have the decompressor entirely configured, it's time
-	// to read out all of the scanlines of the jpeg.
-	//
-	// By default, scanlines will come out in RGBRGBRGB...  order, 
-	// but this can be changed by setting cinfo.out_color_space
-	//
-	// jpeg_read_scanlines takes an array of buffers, one for each scanline.
-	// Even if you give it a complete set of buffers for the whole image,
-	// it will only ever decompress a few lines at a time. For best 
-	// performance, you should pass it an array with cinfo.rec_outbuf_height
-	// scanline buffers. rec_outbuf_height is typically 1, 2, or 4, and 
-	// at the default high quality decompression setting is always 1.
 	while (cinfo.output_scanline < cinfo.output_height) {
 		unsigned char *buffer_array[1];
 		buffer_array[0] = bmp_buffer + (cinfo.output_scanline) * row_stride;
@@ -145,36 +104,23 @@ int main (int argc, char *argv[]) {
 		jpeg_read_scanlines(&cinfo, buffer_array, 1);
 
 	}
-	syslog(LOG_INFO, "Proc: Done reading scanlines");
 
-
-	// Once done reading *all* scanlines, release all internal buffers,
-	// etc by calling jpeg_finish_decompress. This lets you go back and
-	// reuse the same cinfo object with the same settings, if you
-	// want to decompress several jpegs in a row.
-	//
-	// If you didn't read all the scanlines, but want to stop early,
-	// you instead need to call jpeg_abort_decompress(&cinfo)
+	// Decompression is finished, change cinfo state to reflect this
 	jpeg_finish_decompress(&cinfo);
-
-	// At this point, optionally go back and either load a new jpg into
-	// the jpg_buffer, or define a new jpeg_mem_src, and then start 
-	// another decompress operation.
 	
-	// Once you're really really done, destroy the object to free everything
+	// Destroy jpeg_decompress object labled cinfo and free buffer memory
 	jpeg_destroy_decompress(&cinfo);
-	// And free the input buffer
 	free(jpg_buffer);
 
 	// Invert colours
 	for (unsigned long x = 0; x < bmp_size; x++) {
-		bmp_buffer[x] ^= 0xFF;
+		//printf("%c ", bmp_buffer[x]);
+		bmp_buffer[x] ^= 0xFF;	// Invert the colours in the image by flipping all bits
 	}
-
 	
 	// Write the decompressed bitmap out to a ppm file, just to make sure 
 	// it worked. 
-	fd = open("output.ppm", O_CREAT | O_WRONLY, 0666);
+	fd = open("output2.ppm", O_CREAT | O_WRONLY, 0666);
 	char buf[1024];
 
 	rc = sprintf(buf, "P6 %d %d 255\n", width, height);
@@ -184,6 +130,5 @@ int main (int argc, char *argv[]) {
 	close(fd);
 	free(bmp_buffer);
 
-	syslog(LOG_INFO, "End of decompression");
-	return EXIT_SUCCESS;
+	return 0;
 }
